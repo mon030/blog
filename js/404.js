bf1b0704b44bc3fa847289b1b083d05aed2f8405
mon030 <42599449+mon030@users.noreply.github.com>
2018-10-03 09:04:50 +0800

    404

---
+++ js/404.js
@@ -0,0 +1,32 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-20961733-1']);
+_gaq.push(['_setSampleRate', '0.1']);
+_gaq.push(['_trackPageview', "/404" + document.location.pathname + document.location.search ]);
+
+(function() {
+ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'https://www') + '.google-analytics.com/ga.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+(function() {
+  var backButton = document.getElementById('js-history-back')
+  backButton.onclick = function() {
+    history.go(-1)
+  }
+}());
+
+(function() {
+  if (zap) {
+    zap.config({
+      'apiHost': 'imon.tk/404',
+      'product': '3ON EM',
+    })
+    zap.trackPageShow()
+    zap.trackMonitor({}, {
+      'monitor': {
+        'statusCode': 404
+      }
+    })
+  }
+}());
